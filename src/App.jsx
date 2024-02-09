71b0476bc9e96434ebe86da60c7e48ce553cf549
Pawan <pawanb78@gmail.com>
2024-02-09 21:29:40 +0000

    react and tailwind css installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h2 className='text-3xl font-bold underline'>Welcome to your Recipe app</h2>
+    </>
+  )
+}
+
+export default App
