bdf469d65ec43756a28c04a4b16f8eb6a3e012ac
Pawan <pawanb78@gmail.com>
2024-02-10 13:59:23 +0000

    successfully fetched data from the api

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,17 +4,15 @@ import {NavLink} from 'react-router-dom';
 import { GlobalContext } from '../context/GlobalContext';
 
 export default function Navbar() {
-  const {searchParam, setsearchParam} = useContext(GlobalContext);
-  console.log(searchParam);
-
-
-
+  const {searchParam, setsearchParam, handleSearch} = useContext(GlobalContext);
+  console.log
+  
   return (
     <nav className='flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0'>
         <h2 className='text-2xl font-semibold'>
           <NavLink to = {'/'} className = "text-black hover:text-gray-700 duration-300">Food Recipe</NavLink>
         </h2>
-        <form>
+        <form onSubmit={handleSearch}>
           <input type="text" name='search' placeholder='Try finding your desired recipe here...' className='bg-white/75 p-3 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200' value={searchParam} onChange= {(e) => setsearchParam(e.target.value)}/>
         </form>
           <ul className='flex gap-5'>
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -6,8 +6,18 @@ export const GlobalContext = createContext(null);
 export default function globalState({children}){
 
     const [searchParam, setsearchParam] = useState('');
+    async function handleSearch(e){
+        e.preventDefault();
+        try{
+            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
+            const data = await res.json();
+            console.log(data);
+        }catch(e){
+            console.log(e);
+        }
+    }
 
     return (
-        <GlobalContext.Provider value={{searchParam, setsearchParam}}>{children}</GlobalContext.Provider>
+        <GlobalContext.Provider value={{searchParam, setsearchParam, handleSearch}}>{children}</GlobalContext.Provider>
     )
 }
